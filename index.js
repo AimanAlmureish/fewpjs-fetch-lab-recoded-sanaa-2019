a26c2d4c1ca18b05de5b84e443ef5716941ee394
Aiman Almureish <aiman.almureish@gmail.com>
2019-10-26 21:28:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ function renderBooks(json) {
     const h2 = document.createElement('h2')
     h2.innerHTML = `<h2>${book.name}</h2>`
     main.appendChild(h2)
+    console.log(book.name);
   })
 }
 
