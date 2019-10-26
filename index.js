e9c0abc258585c8c5af1e4e79631a8a438a348c0
Aiman Almureish <aiman.almureish@gmail.com>
2019-10-26 21:29:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function fetchBooks() {
     fetch('https://anapioficeandfire.com/api/books')
       .then(resp => resp.json())
-      .then(json => console.log(josn))
+      .then(json => console.log(json))
 }
 
 function renderBooks(json) {
