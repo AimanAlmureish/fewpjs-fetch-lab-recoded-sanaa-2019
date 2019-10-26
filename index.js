4aaefb891cb61ee45cefb11dc8e98f8e1fdd97ad
Aiman Almureish <aiman.almureish@gmail.com>
2019-10-26 21:36:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function renderBooks(json) {
   json.forEach(book => {
     const h2 = document.createElement('h2')
     h2.innerHTML = `<h2>${book.name}</h2>`
-    main.appendChild(h2)
+    main.appendChild(h2) 
     console.log(book.name);
   })
 }
